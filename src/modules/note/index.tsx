193dec548d40011fc14bafd194627002cb629f4e
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-12-26 23:18:30 +0200

    created basic editor

--- src/modules/note/index.tsx
+++ src/modules/note/index.tsx
@@ -17,8 +17,25 @@ import {
 	InsertImage,
 	InsertTable,
 	InsertThematicBreak,
+	InsertCodeBlock,
 	ListsToggle,
+	codeBlockPlugin,
+	codeMirrorPlugin,
+	imagePlugin,
 } from "@mdxeditor/editor";
+
+async function imageUploadHandler(image: File) {
+	const formData = new FormData();
+	formData.append("image", image);
+	// send the file to your server and return
+	// the URL of the uploaded image in the response
+	const response = await fetch("/uploads/new", {
+		method: "POST",
+		body: formData,
+	});
+	const json = (await response.json()) as { url: string };
+	return json.url;
+}
 
 function App() {
 	return (
@@ -33,14 +50,26 @@ function App() {
 							<UndoRedo />
 							<BoldItalicUnderlineToggles />
 							<BlockTypeSelect />
-							<CodeToggle />
 							<InsertImage />
 							<CreateLink />
 							<InsertTable />
 							<InsertThematicBreak />
-							<ListsToggle/>
+							<ListsToggle />
+							<InsertCodeBlock />
 						</>
 					),
+				}),
+				imagePlugin({ imageUploadHandler }),
+				codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+				codeMirrorPlugin({
+					codeBlockLanguages: {
+						js: "JavaScript",
+						ts: "Typescript",
+						tsx: "React TSX",
+						json: "JSON",
+						diff: "Diff",
+						typescript: "Typescript",
+					},
 				}),
 				headingsPlugin(),
 				listsPlugin(),
