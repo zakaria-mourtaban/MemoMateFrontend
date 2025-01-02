9ad66f5662f2af413915698481e2b7539c8f5589
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-02 20:49:48 +0200

    styled sign up page logo and moto

--- src/modules/auth/signup.tsx
+++ src/modules/auth/signup.tsx
@@ -1,41 +1,49 @@
 import Logo from "../core/components/logo";
-import "./styles/signup.css"
+import "./styles/signup.css";
 
 const Signup = () => {
 	return (
 		<div className="signup-container">
 			<div className="signup">
-				<div className="logo">
-					<Logo />
+				<div>
+					<div className="signup-logo">
+						<Logo />
+					</div>
+					<div>
+						<div className="signup-headline">
+							<h1>Sign Up</h1>
+						</div>
+						<div className="signup-subtext">
+							<p>
+								The first step to becoming one with the notepad,
+								to engage in a natural and fulfilling note
+								taking environment
+							</p>
+						</div>
+					</div>
 				</div>
-				<div className="headline">
-					<h1>Sign Up</h1>
+				<div>
+					<div className="input">
+						<p>Username</p>
+						<input type="Name" placeholder="arcticpoet76" />
+					</div>
+					<div className="input">
+						<p>Email</p>
+						<input type="Email" placeholder="email@example.com" />
+					</div>
+					<div className="input">
+						<p>Password</p>
+						<input type="Password" placeholder="************" />
+					</div>
 				</div>
-				<div className="subtext">
-					<h5>
-						The first step to becoming one with the notepad, to
-						engage in a natural and fulfilling note taking
-						environment
-					</h5>
-				</div>
-				<div className="input">
-					<h6>Username</h6>
-					<input type="Name" placeholder="arcticpoet76" />
-				</div>
-				<div className="input">
-					<h6>Email</h6>
-					<input type="Email" placeholder="email@example.com" />
-				</div>
-				<div className="input">
-					<h6>Password</h6>
-					<input type="Password" placeholder="************" />
-				</div>
-				<div className="signupbtn">
-					<button>Signup</button>
-				</div>
-				<div className="redirect">
-					<div>Already have and account?{" "}</div>
-					<div className="redirect-link">Sign in</div>
+				<div className="signup-submit">
+					<div className="signup-btn">
+						<button>Signup</button>
+					</div>
+					<div className="redirect">
+						<div>Already have and account? </div>
+						<div className="redirect-link">Sign in</div>
+					</div>
 				</div>
 			</div>
 			<div className="demo"></div>
--- src/modules/core/components/logo.tsx
+++ src/modules/core/components/logo.tsx
@@ -3,6 +3,7 @@ import "./logo.css";
 const Logo = () => {
 	return (
 		<img
+			className="image"
 			src={require("../../../assets/icons/Zakaria Mourtaban_NoText.png")}
 			alt="Logo"
 		/>
