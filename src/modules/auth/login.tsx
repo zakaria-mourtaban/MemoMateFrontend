159eee370a665530903a30feb1a64e296d5e1eda
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-02 19:11:11 +0200

    created nested login page divs

--- src/modules/auth/login.tsx
+++ src/modules/auth/login.tsx
@@ -2,9 +2,17 @@ const Login = () => {
 	return (
 		<div className="login-container">
 			<div className="demo"></div>
-			<div className="login"></div>
+			<div className="login">
+				<div className="logo"></div>
+				<div className="headline"></div>
+				<div className="subtext"></div>
+				<div className="input"></div>
+				<div className="input"></div>
+				<div className="loginbtn"></div>
+				<div className="redirect"></div>
+			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Login
+export default Login;
