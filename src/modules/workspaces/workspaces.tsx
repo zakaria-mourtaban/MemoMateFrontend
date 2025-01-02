8def6eb82b005c4455130db4964166b558778571
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-03 00:11:20 +0200

    created workspaces page

--- src/modules/workspaces/workspaces.tsx
+++ src/modules/workspaces/workspaces.tsx
@@ -1,7 +1,35 @@
-const workspaces = () => {
-	return (
-		<div className="workspaces"></div>
-	)
+// Workspaces.js
+import React, { useState } from 'react';
+import Navbar from '../core/components/navbar';
+
+const Workspaces = () => {
+    const [workspaces, setWorkspaces] = useState([]);
+
+    const addWorkspace = () => {
+        const newWorkspace = { id: Date.now(), name: `Workspace ${workspaces.length + 1}` };
+        setWorkspaces([...workspaces, newWorkspace]);
+    }
+
+    const deleteWorkspace = (id) => {
+        setWorkspaces(workspaces.filter(workspace => workspace.id !== id));
+    }
+
+    return (
+        <div className="workspaces-container">
+            <Navbar />
+            <div className="workspaces-header">
+                <button className="workspaces-add-button" onClick={addWorkspace}>+</button>
+            </div>
+            <div className="workspaces-list">
+                {workspaces.map(workspace => (
+                    <div key={workspace.id} className="workspaces-item">
+                        <span>{workspace.name}</span>
+                        <button className="workspaces-delete-button" onClick={() => deleteWorkspace(workspace.id)}>Delete</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default workspaces
+export default Workspaces;
