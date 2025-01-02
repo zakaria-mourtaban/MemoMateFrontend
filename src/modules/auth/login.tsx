60e4667153ba395e6d6714809c1e521632dbbb76
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-02 22:49:47 +0200

    added login page and styling

--- src/modules/auth/login.tsx
+++ src/modules/auth/login.tsx
@@ -1,15 +1,47 @@
+import Logo from "../core/components/logo";
+import "./styles/login.css";
+
 const Login = () => {
 	return (
 		<div className="login-container">
 			<div className="demo"></div>
 			<div className="login">
-				<div className="logo"></div>
-				<div className="headline"></div>
-				<div className="subtext"></div>
-				<div className="input"></div>
-				<div className="input"></div>
-				<div className="loginbtn"></div>
-				<div className="redirect"></div>
+				<div>
+					<div className="login-logo">
+						<Logo />
+					</div>
+					<div>
+						<div className="login-headline">
+							<h1>Login</h1>
+						</div>
+						<div className="login-subtext">
+							<p>
+								Log in to become one with the notepad, to engage
+								in a natural and fulfilling note taking
+								environment
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="inputs">
+					<div className="input">
+						<p>Email</p>
+						<input type="Email" placeholder="email@example.com" />
+					</div>
+					<div className="input">
+						<p>Password</p>
+						<input type="Password" placeholder="••••••••••••" />
+					</div>
+				</div>
+				<div className="login-submit">
+					<div className="login-btn">
+						<button>Login</button>
+					</div>
+					<div className="redirect">
+						<div>Don't have and account? </div>
+						<div className="redirect-link">Sign Up</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
