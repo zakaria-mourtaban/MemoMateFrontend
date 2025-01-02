86d69bb2780222e51ae96aec6e07ecba0d3b18cf
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-02 19:26:23 +0200

    created login div layout

--- src/modules/auth/signup.tsx
+++ src/modules/auth/signup.tsx
@@ -1,14 +1,41 @@
+import Logo from "../core/components/logo";
+
 const Signup = () => {
 	return (
 		<div className="signup-container">
 			<div className="signup">
-				<div className="logo"></div>
-				<div className="headline"></div>
-				<div className="subtext"></div>
-				<div className="input"></div>
-				<div className="input"></div>
-				<div className="signupbtn"></div>
-				<div className="redirect"></div>
+				<div className="logo">
+					<Logo />
+				</div>
+				<div className="headline">
+					<h1>Sign Up</h1>
+				</div>
+				<div className="subtext">
+					<h5>
+						The first step to becoming one with the notepad, to
+						engage in a natural and fulfilling note taking
+						environment
+					</h5>
+				</div>
+				<div className="input">
+					<h6>Username</h6>
+					<input type="Name" placeholder="arcticpoet76" />
+				</div>
+				<div className="input">
+					<h6>Email</h6>
+					<input type="Email" placeholder="email@example.com" />
+				</div>
+				<div className="input">
+					<h6>Password</h6>
+					<input type="Password" placeholder="************" />
+				</div>
+				<div className="signupbtn">
+					<button>Signup</button>
+				</div>
+				<div className="redirect">
+					<div>Already have and account?{" "}</div>
+					<div className="redirect-link">Sign in</div>
+				</div>
 			</div>
 			<div className="demo"></div>
 		</div>
