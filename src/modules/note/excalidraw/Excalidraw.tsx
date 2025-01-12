6c9786b87f9c6b82c930da8459fb8cbfbf0b5771
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-13 01:27:34 +0200

    reverted changes to excalidraw component

--- src/modules/note/excalidraw/Excalidraw.tsx
+++ src/modules/note/excalidraw/Excalidraw.tsx
@@ -3,7 +3,46 @@ import { Excalidraw, MainMenu } from "@excalidraw/excalidraw";
 import "../styles/excalidraw.css";
 
 const ExcalidrawComponent = () => {
-	const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+	//   const excalidrawRef = useRef(null);
+	// const [theme, setTheme] = useState<"light" | "dark">("light");
+	// const [viewModeEnabled, setViewModeEnabled] = useState(false);
+	// const [gridVisible, setGridVisible] = useState(true);
+
+	//   const handleSave = () => {
+	//     if (excalidrawRef.current) {
+	//       const sceneData = excalidrawRef.current.getSceneElements();
+	//       console.log("Scene Data:", sceneData);
+	//       // Save the sceneData to a server or localStorage
+	//     }
+	//   };
+
+	//   const handleLoadExample = () => {
+	//     const exampleScene = {
+	//       elements: [
+	//         {
+	//           id: "rect1",
+	//           type: "rectangle",
+	//           x: 100,
+	//           y: 100,
+	//           width: 200,
+	//           height: 100,
+	//           strokeColor: "#000000",
+	//           backgroundColor: "#ffffff",
+	//           fillStyle: "solid",
+	//           strokeWidth: 1,
+	//           roughness: 1,
+	//           opacity: 100,
+	//         },
+	//       ],
+	//       appState: {
+	//         viewBackgroundColor: "#f0f0f0",
+	//         gridSize: 20,
+	//       },
+	//     };
+
+	//     excalidrawRef.current.updateScene(exampleScene);
+	//   };
+
 	return (
 		<div
 			style={{
@@ -14,8 +53,20 @@ const ExcalidrawComponent = () => {
 		>
 			<div style={{ flex: 1 }}>
 				<Excalidraw
-					excalidrawAPI={(api)=> setExcalidrawAPI(api)}
 					validateEmbeddable={() => true}
+					// theme={theme}
+					// viewModeEnabled={viewModeEnabled}
+					// gridModeEnabled={gridVisible}
+					// onChange={(elements, state) => {
+					// 	console.log("Elements:", elements, "State:", state);
+					// }}
+					initialData={{
+						elements: [],
+						appState: {
+							viewBackgroundColor: "#ffffff",
+							gridSize: 20,
+						},
+					}}
 					name="Excalidraw Canvas"
 					UIOptions={{
 						canvasActions: {
@@ -27,7 +78,6 @@ const ExcalidrawComponent = () => {
 							toggleTheme: true,
 						},
 					}}
-					onChange={()=>{console.log(excalidrawAPI)}}
 				>
 					<MainMenu>
 						<MainMenu.DefaultItems.LoadScene></MainMenu.DefaultItems.LoadScene>
