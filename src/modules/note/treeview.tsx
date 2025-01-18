8d717eaaae528a8ee2f1804c67b6b83d040583e2
zakaria-mourtaban <zakariamourtaban@gmail.com>
2025-01-18 22:19:23 +0200

    created folder add function

--- src/modules/note/treeview.tsx
+++ src/modules/note/treeview.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Tree, TreeApi } from "react-arborist";
 import Node from "./node";
 import "./styles/treeview.css";
-import FormData from "form-data"
+import FormData from "form-data";
 import {
 	ChevronsDownUp,
 	ChevronsUpDown,
@@ -16,7 +16,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState, setCollapsed, setCurrentNode } from "../../store/store";
 import axios from "axios";
 import { apiCall, getTokenFromCookie } from "../core/utils/api";
-import fs from "fs"
+import fs from "fs";
 import Swal from "sweetalert2";
 
 interface FileNode {
@@ -111,99 +111,94 @@ const FileTreeView: React.FC<FileTreeViewProps> = ({ data, load }) => {
 
 	const handleFileAdd = async () => {
 		try {
-		  const jsonData = {
-			type: "excalidraw",
-			version: 2,
-			source: "http://localhost:3000",
-			elements: [],
-			appState: {
-			  gridSize: 20,
-			  viewBackgroundColor: "#ffffff",
-			},
-			files: {},
-		  };
-	  
-		  const { value: fileName } = await Swal.fire({
-			title: "Enter a file name",
-			input: "text",
-			inputLabel: "File Name",
-			inputPlaceholder: "Enter the name for the file",
-			showCancelButton: true,
-			confirmButtonText: "Upload",
-			inputValidator: (value) => {
-			  if (!value) return "You need to enter a file name!";
-			},
-		  });
-	  
-		  if (!fileName) return;
-	  
-		  const jsonString = JSON.stringify(jsonData, null, 2);
-		  const jsonFile = new Blob([jsonString], { type: "application/json" });
-	  
-		  const formData = new FormData();
-		  formData.append("file", jsonFile, `${fileName}.json`);
-		  formData.append("name", fileName);
-	  
-		  const token = getTokenFromCookie();
-	  
-			console.log(currentWorkspace)
-		  const response = await axios.post(
-			`http://localhost:5000/api/workspace/${currentNode?.id ? currentNode.id : currentWorkspace?._id}/add`,
-			formData,
-			{
-			  headers: {
-				Authorization: `Bearer ${token}`,
-				...formData.getHeaders?.(),
-			  },
-			}
-		  );
-	  
-		  load();
-		  return response;
+			const jsonData = {
+				type: "excalidraw",
+				version: 2,
+				source: "http://localhost:3000",
+				elements: [],
+				appState: {
+					gridSize: 20,
+					viewBackgroundColor: "#ffffff",
+				},
+				files: {},
+			};
+
+			const { value: fileName } = await Swal.fire({
+				title: "Enter a file name",
+				input: "text",
+				inputLabel: "File Name",
+				inputPlaceholder: "Enter the name for the file",
+				showCancelButton: true,
+				confirmButtonText: "Upload",
+				inputValidator: (value) => {
+					if (!value) return "You need to enter a file name!";
+				},
+			});
+
+			if (!fileName) return;
+
+			const jsonString = JSON.stringify(jsonData, null, 2);
+			const jsonFile = new Blob([jsonString], {
+				type: "application/json",
+			});
+
+			const formData = new FormData();
+			formData.append("file", jsonFile, `${fileName}.json`);
+			formData.append("name", fileName);
+
+			const token = getTokenFromCookie();
+
+			console.log(currentWorkspace);
+			const response = await axios.post(
+				`http://localhost:5000/api/workspace/${
+					currentNode?.id ? currentNode.id : currentWorkspace?._id
+				}/add`,
+				formData,
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+						...formData.getHeaders?.(),
+					},
+				}
+			);
+
+			load();
+			return response;
 		} catch (error) {
-		  console.error(error);
-		  await Swal.fire({
-			icon: "error",
-			title: "Upload Failed",
-			text: "An error occurred while uploading the file.",
-		  });
+			console.error(error);
+			await Swal.fire({
+				icon: "error",
+				title: "Upload Failed",
+				text: "An error occurred while uploading the file.",
+			});
 		}
-	  };
-	  
+	};
+
 	const handleFolderAdd = async () => {
 		try {
 			const formData = new FormData();
-			const fileInput = document.createElement("input");
-			fileInput.type = "file";
-			fileInput.accept = ".pdf,.docx,.excalidraw,.txt";
 
-			const fileSelected = new Promise<File>((resolve, reject) => {
-				fileInput.onchange = (event: Event) => {
-					const target = event.target as HTMLInputElement;
-					const selectedFile = target.files?.[0];
-					if (selectedFile) {
-						resolve(selectedFile);
-					} else {
-						reject(new Error("No file selected"));
-					}
-				};
-
-				fileInput.oncancel = () => {
-					reject();
-				};
+			const { value: fileName } = await Swal.fire({
+				title: "Enter a folder name",
+				input: "text",
+				inputLabel: "File Name",
+				inputPlaceholder: "Enter the name for the folder",
+				showCancelButton: true,
+				confirmButtonText: "Upload",
+				inputValidator: (value) => {
+					if (!value) return "You need to enter a file name!";
+				},
 			});
 
-			fileInput.click();
-			const file = await fileSelected;
-
-			formData.append("file", file);
-			formData.append("name", file.name);
+			if (!fileName) return;
+			formData.append("file", null);
+			formData.append("name", fileName);
 
 			const token = getTokenFromCookie();
 			const response = await axios({
 				method: "POST",
 				url: `http://localhost:5000/api/workspace/${
-					currentNode.id ? currentNode.id : currentWorkspace?._id
+					currentNode?.id ? currentNode.id : currentWorkspace?._id
 				}/add`,
 				data: formData,
 				headers: {
@@ -221,7 +216,7 @@ const FileTreeView: React.FC<FileTreeViewProps> = ({ data, load }) => {
 				<button onClick={handleFileAdd}>
 					<FilePlus2 size={20} />
 				</button>
-				<button onClick={() => {}}>
+				<button onClick={handleFolderAdd}>
 					<FolderPlus size={20} />
 				</button>
 				<button onClick={alterAll}>
